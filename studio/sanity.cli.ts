28d81c95fd9cd25dc055a6ad281b516528190940
sholadoye najib <najibsholadoye04@gmail.com>
2026-08-04 18:23:35 +0100

    fix: sanity deploy

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,11 +1,14 @@
 import { defineCliConfig } from 'sanity/cli';
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'your-project-id';
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'ya1nxsau';
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production';
 
 export default defineCliConfig({
   api: {
     projectId: projectId,
     dataset: dataset
+  },
+  deployment: {
+    appId: 'z9193yf96gip8l8b1qc58pny'
   }
 });
--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -3,7 +3,7 @@ import { structureTool } from 'sanity/structure';
 import { schemaTypes } from './schemaTypes';
 
 // For Sanity Studio v3, env variables must be prefixed with SANITY_STUDIO_
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'your-project-id';
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'ya1nxsau';
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production';
 
 export default defineConfig({
